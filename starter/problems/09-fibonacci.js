75aea823968dc761d3d50c58843ac7f7d8067edb
ethanchen7 <ethan.chen7@gmail.com>
2022-02-01 13:53:54 -0800

    fibonacci

--- starter/problems/09-fibonacci.js
+++ starter/problems/09-fibonacci.js
@@ -22,11 +22,19 @@ fibonacci(10); // 55
 // your code here
 
 const fib = (num) => {
-  if (num <= 0) return null;
+  if (num <= 0) return 0;
   if (num === 1 || num === 2) return 1;
 
-  return 1 + fib(num - 1);
+  // 4
+  return fib(num - 2) + fib(num - 1);
+  //fib( 2 )      // fib ( 3 )
+  // fib ( 3 - 2 ) + fib ( 3 - 1 )
+  // fib ( 1 )  + fib ( 2 )
+  //        1                           ( 1 + 1 )
 };
+
+console.log(fib(3)); // 2
+console.log(fib(10)); // 3
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
