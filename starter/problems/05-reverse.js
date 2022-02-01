9eb5b52ee82c6540cbb0169e854e2c8a3a0a480c
ethanchen7 <ethan.chen7@gmail.com>
2022-02-01 10:29:59 -0800

    fifth

--- starter/problems/05-reverse.js
+++ starter/problems/05-reverse.js
@@ -14,6 +14,25 @@ reverse(""); // ""
 
 // your code here
 
+const reverse = (str) => {
+  if (str.length === 0) {
+    return "";
+  }
+
+  if (str.length === 1) {
+    return str;
+  }
+  // ['h','o','u','s','e']
+  let array = str.split("");
+  let newFirst = array.pop();
+  let newString = "";
+  if (newString.length < str.length) {
+    newString += newFirst;
+    //esuoh
+  }
+  return reverse(str);
+};
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = reverse;
