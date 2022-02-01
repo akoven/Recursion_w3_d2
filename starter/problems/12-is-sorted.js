ff04e50d955d5ee2835f714cb05f2d357f458ab7
ethanchen7 <ethan.chen7@gmail.com>
2022-02-01 14:28:34 -0800

    isSorted

--- starter/problems/12-is-sorted.js
+++ starter/problems/12-is-sorted.js
@@ -12,6 +12,17 @@ isSorted([5, 4, 3, 2, 1]); // false
 
 // your code here
 
+const isSorted = function (arr) {
+  if (arr.length <= 1) return true;
+  //[ 2, 3, 4, 5]
+  let num1 = arr.shift(); // 1
+  // 1 < 2
+  if (num1 < arr[0]) {
+    return isSorted(arr);
+  } else {
+    return false;
+  }
+};
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = isSorted;
