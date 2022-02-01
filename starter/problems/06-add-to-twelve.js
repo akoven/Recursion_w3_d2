8c67bbec998a871e0fdc65ffda5a5ff3e7389ecd
Attiya <adarensburg@live.com>
2022-02-01 14:32:55 -0600

    commiting changes to problem 6

--- starter/problems/06-add-to-twelve.js
+++ starter/problems/06-add-to-twelve.js
@@ -11,9 +11,23 @@ addToTwelve([1, 11, 4, 7, 6]); // true
 addToTwelve([1, 12, 4, 7, 6]); // false
 addToTwelve([1]); // false
 ***********************************************************************/
+const addToTwelve = (arr) =>{
+  // if(arr.length <= 1){
+  //   return false;
+  // }
 
-// your code here
+  // let index = arr.length - 1;
+  if(arr.length !== 0){
+    arr.forEach((ele,i) => {
+      return (ele+arr[i+1] === 12)
+    });
+    let shiftedArr = arr.slice(2);
+    // return shiftedArr;
+    return addToTwelve(shiftedArr);
+  }
+}
 
+console.log(addToTwelve([1, 3, 4, 7, 5])); // true
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = addToTwelve;
